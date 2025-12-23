b0091ad8b201f9225e324d2bccf7b2acc40f8c34
rashel9255 <ibrahim2000167@gmail.com>
2025-12-23 15:28:45 +0600

    feat: fetch all foods data

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Foods = () => {
+const Foods = async() => {
+
+    const res = await fetch("https://taxi-kitchen-api.vercel.app/api/v1/foods/random", {
+        cache: "no-store"
+    });
+    const data = await res.json();
+    const allFoods = data.foods;
+    console.log(allFoods);
+
+
     return (
         <div>
-           This is foods page
+           <h2>Total Foods: {allFoods.length}</h2>
         </div>
     );
 };
